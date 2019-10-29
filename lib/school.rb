8aac1427ff1a6871ac257ffb63afdf665f1a9fc8
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-29 01:31:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end 
+  def add_student(name, grade)
+    #binding.pry
+    if roster[grade]
+       roster[grade] << name 
+    else 
+       roster[grade] = [] 
+       roster[grade] << name 
+    end 
+   
+  end
+  
+end 
